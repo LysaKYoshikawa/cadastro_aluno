2846cd6c00e147eb9c6509628ca3b7d00ddb6b60
monalysa <monalysaklauck@gmail.com>
2021-07-13 02:04:46 -0300

    atualizacao de cod 13.07

--- src/Dominio/Professor.java
+++ src/Dominio/Professor.java
@@ -1,12 +1,14 @@
 package Dominio;
 
+import auxiliar.Constante;
+
 public class Professor {
 
-    public String nomeDoProfessor;
-    public int turmaDoProfessor;
-    public  float salarioDoProfessor;
-    public boolean posGraduado;
-    public String especializacao;
+    private String nomeDoProfessor;
+    private int turmaDoProfessor;
+    private float salarioDoProfessor;
+    private boolean posGraduado;
+    private String especializacao;
 
     public Professor(String nomeDoProfessor,int turmaDoProfessor){
         this.nomeDoProfessor=nomeDoProfessor; //esse this é para referenciar que ele é a String da logica.
@@ -15,19 +17,81 @@ public class Professor {
 
     }
 
-    public Professor(String nomeDoProfessor,int turmaDoProfessor,float salarioDoProfessor, boolean posGraduado,String especializacao){
-        this.nomeDoProfessor=nomeDoProfessor;
-        this.turmaDoProfessor=turmaDoProfessor;
+    public Professor(String nomeDoProfessor, int turmaDoProfessor, float salarioDoProfessor, boolean posGraduado, String especializacao){
+        this(nomeDoProfessor,turmaDoProfessor);
+        //this.nomeDoProfessor=nomeDoProfessor;
+        //this.turmaDoProfessor=turmaDoProfessor;
         this.salarioDoProfessor=salarioDoProfessor;
         this.posGraduado=posGraduado;
         this.especializacao=especializacao;
 
     }
 
+    private float calculadoraComissaoPosGraduado(){
+        return posGraduado ? 1500 : 500;
+    }
+
+    private float calcularComissaoEspecializacao(){
+        return "Projeto".equalsIgnoreCase(especializacao ) ? 2000 : 500;
+    }
+
+    private float calcularSalario(){
+        return salarioDoProfessor + calculadoraComissaoPosGraduado() + calcularComissaoEspecializacao();
+    }
+
+    private String getSituacaoProfessor(float calcularSalario){
+        if (calcularSalario > 3500){
+            return "Professor qualificado";
+        } else if (calcularSalario < 1500){
+            return "Professor regular";
+        } else {
+            return "Professor não qualificado";
+        }
+
+
+    }
+
     @Override // esse @ vem para informar que ele é um metodo que estamos sobreescrevendo em
     public String toString(){
-        return "Sou a Professora" + " " + this.nomeDoProfessor + " " + "estou na turma" + " " + this.turmaDoProfessor + " " + "Pos graduado "
-                + " " + this.posGraduado + " " + "no curso de " + " " + this.especializacao + "recebendo o salario no valor de: " + this.salarioDoProfessor;
+        float sl = calcularSalario();
+        return "Sou a Professora" + " " + this.nomeDoProfessor + " " + "estou na turma" +
+                " " + this.turmaDoProfessor + " " + "Pos graduado " + " " + this.posGraduado +
+                " " + "no curso de " + " " + this.especializacao + " " +
+                "recebendo o salario no valor de: " + this.salarioDoProfessor +
+                "||" + sl + "(" + getSituacaoProfessor(sl)+ ")";
     }
+ // private String especializacao
+    //get
+    public String getEspecializacao(){
+        return this.especializacao;
+
+    }
+    //set
+    public void setEspecializacao(String projetos){
+        this.especializacao=especializacao;
+
+    }
+    
+    //get
+    public float getSalarioDoProfessor(){
+        return this.salarioDoProfessor;
+
+    }
+    //set
+    public void setSalarioDoProfessor(float salarioDoProfessor){
+        this.salarioDoProfessor=salarioDoProfessor;
+
+    }
+    //get
+    public Boolean getPosGraducao(){
+        return this.posGraduado;
+
+    }
+    //set
+    public void setPosGraducao(boolean posGraduado){
+        this.posGraduado=posGraduado;
+
+    }
+
 
 }
--- src/cadastro/ProfessorTeste.java
+++ src/cadastro/ProfessorTeste.java
@@ -6,14 +6,20 @@ public class ProfessorTeste {
 
     public static void main(String[] args) {
 
-        Professor prof = new Professor();
+        Professor profe = new Professor("Monalysa",210);
 
-        prof.nomeDoProfessor = "Monalysa";
-        prof.especializacao = "Gestão de Projetos";
-        prof.salarioDoProfessor = 19890;
-        prof.turmaDoProfessor = 210;
-        prof.posGraduado= false;
+        profe.setEspecializacao("Projetos");
+        profe.setSalarioDoProfessor(19890);
+        profe.setPosGraducao(false);
+        System.out.println("Especialização" + profe.getEspecializacao());
+        System.out.println("Salario" + profe.getSalarioDoProfessor());
+        System.out.println("Pos Graduacao" + profe.getPosGraducao());
 
-        System.out.println(prof);
+        System.out.println(profe);
+
+        Professor profx = new Professor("Tiago", 210, 1250,true,"ghdgjd");
+        System.out.println(profx);
     }
+
+
 }
